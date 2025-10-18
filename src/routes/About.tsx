1529903b4a7a0bf0a40d3b07dce0dfd764fd421b
0Ak-JRW <jrw.o5980@gmail.com>
2025-10-18 12:57:44 +0700

    fix: Update About Us heading to include exclamation mark

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 export default function About() {
   return (
     <div className="">
-        <h1 className="text-3xl font-bold underline">About Us</h1>
+        <h1 className="text-3xl font-bold underline">About Us!</h1>
     </div>
   );
 }
