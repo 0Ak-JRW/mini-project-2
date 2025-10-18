be751c20c8855863bf0c9bbbcf3c5086cf23bd36
Folk <sittisak.putthimit@gmail.com>
2025-10-18 15:22:02 +0700

    Refactor code structure for improved readability and maintainability

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -2,8 +2,14 @@ import { useState } from "react";
 
 export default function About() {
   return (
-    <div className="">
-        <h1 className="text-3xl font-bold underline">About Us!</h1>
+    <div className="flex flex-col items-center justify-center py-2">
+      <h1 className="text-center text-3xl font-bold">About Company</h1>
+      <div className="mt-6 flex flex-col items-center">
+        <img src="/src/images/unnamed.webp" alt="Company Logo" className="company-logo rounded-3xl shadow-[0_4px_30px_#00FFE9]" />
+      </div>
+      <div className="mt-6 text-start">
+        <p className="text-lg">Pixelnetwork Co., Ltd.</p>
+      </div>
     </div>
   );
 }
